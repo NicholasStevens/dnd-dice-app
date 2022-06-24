906a1876d4b40df842a32ebe9931873232ae0eb9
SharkWolfMan <nick.stevens.86@gmail.com>
2022-06-24 12:31:24 +0200

    'setup,logic,basic layout '

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [diceRoll, setDiceRoll] = useState(0);
+
+  const handleClick20 = () => {
+    setDiceRoll(Math.floor(Math.random() * 20 + 1));
+  };
+  const handleClick12 = () => {
+    setDiceRoll(Math.floor(Math.random() * 12 + 1));
+  };
+  const handleClick10 = () => {
+    setDiceRoll(Math.floor(Math.random() * 10 + 1));
+  };
+  const handleClick8 = () => {
+    setDiceRoll(Math.floor(Math.random() * 8 + 1));
+  };
+  const handleClick6 = () => {
+    setDiceRoll(Math.floor(Math.random() * 6 + 1));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Throw The Bones</h1>
       </header>
+      <main className="container">
+        <div className="result">{diceRoll}</div>
+
+        <div className="dice">
+          <button onClick={handleClick20} className="dice dice20">
+            20 side
+          </button>
+          <button onClick={handleClick12} className="dice dice12">
+            12 side
+          </button>
+          <button onClick={handleClick10} className="dice dice10">
+            10 side
+          </button>
+          <button onClick={handleClick8} className="dice dice8">
+            8 side
+          </button>
+          <button onClick={handleClick6} className="dice dice6">
+            6 side
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
